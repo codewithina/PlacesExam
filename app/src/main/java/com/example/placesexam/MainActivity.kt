53291f835ec9ad022966b7637a7b896bd5fc95d9
Ina Burström <inaburstrom@hotmail.com>
2024-01-05 12:31:54 +0100

    Added fragment to add new spot.

--- app/src/main/java/com/example/placesexam/MainActivity.kt
+++ app/src/main/java/com/example/placesexam/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.placesexam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var addNewSpot: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       addNewSpot = findViewById(R.id.addNewSpot)
+
+        addNewSpot.setOnClickListener{
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentHolder, AddNewSpotFragment())
+            transaction.commit()
+        }
+
+
     }
 }
