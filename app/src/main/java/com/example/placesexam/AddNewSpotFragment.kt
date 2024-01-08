b482692b16124dba0ba8bd1eda90400e9a903f4f
Ina Burström <inaburstrom@hotmail.com>
2024-01-08 22:16:02 +0100

    Add place can now save to FIrestore.

--- app/src/main/java/com/example/placesexam/AddNewSpotFragment.kt
+++ app/src/main/java/com/example/placesexam/AddNewSpotFragment.kt
@@ -1,59 +1,74 @@
 package com.example.placesexam
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddNewSpotFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.firestore.firestore
+import com.google.firebase.storage.FirebaseStorage
+
 class AddNewSpotFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var imageUrl: String? = null
+    lateinit var image: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_new_spot, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddNewSpot.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddNewSpotFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val saveButton: Button = view.findViewById(R.id.buttonAdd)
+        image = view.findViewById(R.id.addImage)
+
+        saveButton.setOnClickListener {
+            val name = view.findViewById<EditText>(R.id.editTextName).text.toString()
+            val description = view.findViewById<EditText>(R.id.editTextDescription).text.toString()
+            val latString = view.findViewById<EditText>(R.id.editTextLat).text.toString()
+            val lat = latString.toDoubleOrNull()
+            val lngString = view.findViewById<EditText>(R.id.editTextLng).text.toString()
+            val lng = lngString.toDoubleOrNull()
+
+            if (lat == null || lng == null) {
+                Toast.makeText(context, "Latitude and longitude must be valid numbers!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            val db = Firebase.firestore
+            val userId = FirebaseAuth.getInstance().currentUser?.uid
+            val newPlaceRef = db.collection("users").document(userId!!).collection("places").document()
+
+            val placeData = hashMapOf(
+                "name" to name,
+                "description" to description,
+                "lat" to lat,
+                "lng" to lng,
+            )
+
+            newPlaceRef.set(placeData).addOnSuccessListener {
+                Toast.makeText(context, "Place data added successfully!", Toast.LENGTH_SHORT).show()
+                requireActivity().supportFragmentManager.beginTransaction().remove(this).commit()
+            }.addOnFailureListener { e ->
+                Toast.makeText(context, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
