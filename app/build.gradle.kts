65646d3175014c0e2b2d7f451006b68aa45c8813
Ina Burström <inaburstrom@hotmail.com>
2024-01-12 17:39:47 +0100

    Login activity layout fixed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("androidx.cardview:cardview:1.0.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
